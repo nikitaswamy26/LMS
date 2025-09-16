005ca38fd2d4e6cef07b64fc4ff783f7a361130d
MayankChandratre1 <mayankmchandratre@gmail.com>
2025-09-16 20:35:11 +0530

    fix names

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -2,14 +2,8 @@ import path from 'path'
 import multer from 'multer'
 
 const upload = multer({
-    dest: "uploads",
+    storage: multer.memoryStorage(),
     limits: { fileSize: 50 * 1024 * 1024 },
-    storage: multer.diskStorage({
-        destination: "uploads",
-        filename: (_req, file, cb) => {
-            cb(null, file.originalname)
-        }
-    }),
     fileFilter: (_req, file, cb) => {
         let ext = path.extname(file.originalname)
 
