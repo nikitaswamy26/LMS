1411903b4258aeb212cb993565d56e47bccb5fd7
MayankChandratre1 <mayankmchandratre@gmail.com>
2025-09-16 20:28:00 +0530

    fix names

--- server/api/server.js
+++ server/api/server.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
-import { connectDb } from './database/db.js'
+import app from "../app.js";
+import { connectDb } from '../database/db.js'
 import { v2 as cloudinary } from 'cloudinary';
 import Razorpay from 'razorpay'
 import serverless from 'serverless-http'
